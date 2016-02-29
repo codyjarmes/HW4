5e39304df98fd20b3eb37414e8d383bb6aa5cdc5
unknown <jarmes@PBB-ITC-C16.iowa.uiowa.edu>
2016-02-28 21:57:10 -0600

    added model, dbConn.properties, and ojdbc6.jar driver

---
+++ src/java/model/ClevelandIndians.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author jarmes
+ */
+public class ClevelandIndians {
+    
+private int PlayerID;
+private String PlayerName;
+private String College;
+private String PlayerPosition;
+private int age;
+private String Hometown;
+
+public ClevelandIndians() {
+        this.PlayerID = 0;
+        this.PlayerName = "";
+        this.College = "";
+        this.PlayerPosition = "";
+        this.age = 0;
+        this.Hometown = "";
+    }
+
+public ClevelandIndians(int PlayerID, String PlayerName, String College, String PlayerPosition, int age, String Hometown) {
+        this.PlayerID = PlayerID;
+        this.PlayerName = PlayerName;
+        this.College = College;
+        this.PlayerPosition = PlayerPosition;
+        this.age = age;
+        this.Hometown = Hometown;
+    }
+
+
+    public int getPlayerID() {
+        return PlayerID;
+    }
+
+    public void setPlayerID(int PlayerID) {
+        this.PlayerID = PlayerID;
+    }
+
+    public String getPlayerName() {
+        return PlayerName;
+    }
+
+    public void setPlayerName(String PlayerName) {
+        this.PlayerName = PlayerName;
+    }
+
+    public String getCollege() {
+        return College;
+    }
+
+    public void setCollege(String College) {
+        this.College = College;
+    }
+
+    public String getPlayerPosition() {
+        return PlayerPosition;
+    }
+
+    public void setPlayerPosition(String PlayerPosition) {
+        this.PlayerPosition = PlayerPosition;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHometown() {
+        return Hometown;
+    }
+
+    public void setHometown(String Hometown) {
+        this.Hometown = Hometown;
+    }
+
+    @Override
+    public String toString() {
+        return "ClevelandIndians{" + "PlayerID=" + PlayerID + ", PlayerName=" + PlayerName + ", College=" + College + ", PlayerPosition=" + PlayerPosition + ", age=" + age + ", Hometown=" + Hometown + '}';
+    }
+
+    
+}
---
+++ web/Index.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : Index
+    Created on : Feb 28, 2016, 9:29:03 PM
+    Author     : jarmes
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Cleveland Indians</title>
+    </head>
+    <body>
+        <h1>Cleveland Indians Database</h1>
+        <a href="read"> View All Players</a>
+    </body>
+</html>
